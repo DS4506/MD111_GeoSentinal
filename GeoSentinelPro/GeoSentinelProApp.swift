67e55a33c4a5c1fcae5c2d2deed432dbc3dd6eb2
DS4506 <davidsmith4506@gmail.com>
2025-10-11 14:26:07 -0500

    Initial Commit

---
+++ GeoSentinelPro/GeoSentinelProApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoSentinelProApp.swift
+//  GeoSentinelPro
+//
+//  Created by Willie Earl on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GeoSentinelProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
